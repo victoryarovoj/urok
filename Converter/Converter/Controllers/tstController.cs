ca2f142cf056a9d1675e31d27c2a42023d27469d
victoryarovoj <victoryarovoj@gmail.com>
2014-11-10 15:13:06 +0200

    7
    7

--- Converter/Converter/Controllers/tstController.cs
+++ Converter/Converter/Controllers/tstController.cs
@@ -44,12 +44,56 @@ namespace Converter.Controllers
             var district1 = from s in contex.Category
                            where s.CategoryId == tmp && s.Id != v
                            select s;
-            //var  = from sc in ska
-            //          where sc.Id!= v
-            //          select sc;
             int a = district1.Count();
             return Json(new SelectList(district1.ToList<Category>(), "Id", "Category1"), JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult List()
+        {
+            List<Category> cat = new List<Category>();
+            foreach (var item in contex.Category)
+            {
+                cat.Add(item);
+            }
+            return View(cat);
+        }
+
+
+        
+        public ActionResult Edit(int Id)
+        {
+            ViewBag.Title = "Edit";
+            var item = contex.Category.Where(model => model.Id == Id).FirstOrDefault<Category>();
+            var rows = from type in contex.CategorySet
+                       select type;
+            ViewData["Types"] = new SelectList(rows, "Id", "Category_Type", contex.CategorySet);
+
+            
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category cat)
+        {
+            ViewBag.Title = "Edit";
+            var item = contex.Category.Where(model => model.Id == cat.Id).FirstOrDefault<Category>();
+            if (item != null)
+            {
+                item.Category1 =cat.Category1;
+                item.Value = cat.Value;
+                
+                int TypeId = Int32.Parse(Request.Form["TypeId"]);
+
+                item.CategoryId = TypeId;
+                contex.SaveChanges();
+            }
+            //var rows = from type in contex.CategorySet
+            //           select type;
+            //ViewData["Types"] = new SelectList(rows, "Id", "Category_Type", contex.CategorySet);
+            
+            return RedirectToAction("List");
+        }
+
 
         [HttpPost]
         public ActionResult Index(string Name, string State, string District, string District1)
@@ -69,8 +113,9 @@ namespace Converter.Controllers
             int i = 0;
             i = int.Parse(Name);
 
-            double res = i * item.Value;
+            double res = (i * item1.Value) * item.Value;
             ViewBag.MyParam = res.ToString();
+
             List<SelectListItem> state = new List<SelectListItem>();
             foreach (var item4 in contex.CategorySet)
             {
@@ -78,6 +123,52 @@ namespace Converter.Controllers
             }
             ViewBag.StateName = new SelectList(state, "Value", "Text");
             return View();
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(string name)
+        {
+            CategorySet catSet = new CategorySet();
+            catSet.Category_Type = name;
+            if (catSet.Category_Type != null)
+            {
+                contex.CategorySet.Add(catSet);
+                contex.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddParams()
+        {
+            var rows = from type in contex.CategorySet
+                       select type;
+            ViewData["Types"] = new SelectList(rows, "Id", "Category_Type", contex.CategorySet);
+
+            var rows1 = from type1 in contex.Category
+                        select type1;
+            var user = contex.Category.Include("CategoryId");
+            ViewData["Types1"] = new SelectList(rows1, "Id", "Category1", contex.Category);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddParams(Category cat)
+        {
+            Category category = new Category();
+            if (cat != null)
+            {
+                category.Category1 = cat.Category1;
+                category.CategoryId = cat.CategoryId;
+                category.Value = cat.Value;
+
+                contex.Category.Add(category);
+                contex.SaveChanges();
+            }
+            return RedirectToAction("List");
         }
 
         //[HttpPost]
