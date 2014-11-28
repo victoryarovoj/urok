fef15f6a80b6cac46f5e87f13e0ed3c458417e3c
victoryarovoj <victoryarovoj@gmail.com>
2014-11-28 16:42:22 +0200

    9
    9

--- Converter/Converter/App_Start/RouteConfig.cs
+++ Converter/Converter/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Converter
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "tst", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
